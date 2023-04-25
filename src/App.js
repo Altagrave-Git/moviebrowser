e2b57cbba844e91958ac8f4ed9bbe8e527260c38
altagrave <damon.j.turcotte@gmail.com>
2023-04-25 01:28:16 -0600

    Format base app

--- src/App.js
+++ src/App.js
@@ -2,21 +2,18 @@ import './App.css';
 import Navbar from "./components/navbar";
 import HomeView from "./components/home";
 import AboutView from './components/about';
-import { BrowserRouter, Routes, Route} from "react-router-dom";
+import { Routes, Route, BrowserRouter} from "react-router-dom";
 
 function App() {
   return (
-    <div>
-      <BrowserRouter>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<HomeView />} />
-          <Route path="/about" element={<AboutView />} />
-        </Routes>
-      </BrowserRouter>
-    </div>
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<HomeView />} />
+        <Route path="/about" element={<AboutView />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
-
 
 export default App;
