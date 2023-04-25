c464cede498394b45f74791b8abc16960af757ec
altagrave <damon.j.turcotte@gmail.com>
2023-04-25 01:28:32 -0600

    Add components

--- src/components/about.js
+++ src/components/about.js
@@ -3,10 +3,23 @@ import Hero from './hero';
 const AboutView = () => {
   return (
     <>
-      <Hero />
-      <h1>About us.</h1>
+      <Hero text="About Us" />
+      <div className="container">
+        <div className="row">
+          <div className="col-lg-8 offset-lg-2 my-4">
+            <p className="lead">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
+              hendrerit, elit at ultricies mollis, ipsum eros rutrum nisl, ac
+              pharetra eros nibh eget eros. Nulla facilisi. Nulla facilisi.
+              Sore mordnila sit teg, elit gnicsipida rutetcesnoc. Cetulop
+              etimorper, tile ta seilppus, srotcurtsni rutetcesnoc. Isilicaf allun. Isilicaf allun. Sore mordnila sit
+              teg, elit gnicsipida rutetcesnoc cetulop etimorper.
+            </p>
+          </div>
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
 export default AboutView;
