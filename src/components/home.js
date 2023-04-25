01d8b8d13c98cd4763446bb1986e3d8746fdb574
altagrave <damon.j.turcotte@gmail.com>
2023-04-25 01:29:55 -0600

    Add components and placeholder text

--- src/components/home.js
+++ src/components/home.js
@@ -4,8 +4,21 @@ import Hero from './hero'
 const HomeView = () => {
   return (
     <>
-      <Hero />
-      <h1>Hello, from React 201!</h1>
+      <Hero text="Welcome to Movie Browser" />
+      <div className="container">
+        <div className="row">
+          <div className="col-lg-8 offset-lg-2 my-4">
+            <p className="lead">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
+              hendrerit, elit at ultricies mollis, ipsum eros rutrum nisl, ac
+              pharetra eros nibh eget eros. Nulla facilisi. Nulla facilisi.
+              Pellentesque habitant morbi tristique senectus et netus et malesuada
+              fames ac turpis egestas. Donec sed diam ut nunc luctus suscipit.
+              Nullam sit amet libero nec nunc ultricies egestas.
+            </p>
+          </div>
+        </div>
+      </div>
     </>
   )
 }
