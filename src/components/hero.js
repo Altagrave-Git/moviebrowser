3d81520a4d5c689b91112694b01c23884e91a92d
altagrave <damon.j.turcotte@gmail.com>
2023-04-25 01:28:47 -0600

    Style hero component

--- src/components/hero.js
+++ src/components/hero.js
@@ -1,16 +1,10 @@
-const Hero = () => {
+const Hero = ({ text }) => {
   return (
     <div className="md:flex rounded-lg bg-dark p-24 justify-center">
-      {/* background color instead of img */}
-      <div className="text-center md:text-left">
-        <h2 className="text-4xl font-bold text-light">
-          Welcome to Movie Browser
-        </h2>
-        <h3 className="text-2xl font-bold text-light">By: Your Name</h3>
-        <h4 className="text-xl font-bold text-light">A React 201 Project</h4>
-        <div className="text-gray-500 text-light">
-          <p>Powered by: React</p>
-        </div>
+      {/* no img */}
+      {/* just text prop styled as a large, light-colored title */}
+      <div className="text-center">
+        <h1 className="text-4xl text-light py-5">{text}</h1>
       </div>
     </div>
   );
