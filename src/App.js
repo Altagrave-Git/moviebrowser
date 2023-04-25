1a756ee019be348888e8e1a6914b5d930e2222a6
altagrave <damon.j.turcotte@gmail.com>
2023-04-25 03:59:24 -0600

    Add initial search keywords

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
-import './App.css';
+import "./App.css";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+import { useState, useEffect } from "react";
 import Navbar from "./components/navbar";
 import HomeView from "./components/home";
-import AboutView from './components/about';
-import { Routes, Route, BrowserRouter} from "react-router-dom";
+import AboutView from "./components/about";
+import SearchView from "./components/search";
 
 function App() {
+
+  {/* set function-based State for entire app */}
+  const [searchResults, setSearchResults] = useState([]);
+  const [searchQuery, setSearchQuery] = useState("");
+
   return (
     <BrowserRouter>
       <Navbar />
       <Routes>
         <Route path="/" element={<HomeView />} />
         <Route path="/about" element={<AboutView />} />
+        <Route path="/search" keyword={searchQuery} searchResults={searchResults} element={<SearchView />} />
       </Routes>
     </BrowserRouter>
   );
