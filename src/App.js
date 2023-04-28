aebd160ee2a9523cf9ebd72765c7ef4aae827914
Damon Turcotte <damon.j.turcotte@gmail.com>
2023-04-28 01:32:01 -0600

    Add search route, ajax

--- src/App.js
+++ src/App.js
@@ -6,19 +6,27 @@ import HomeView from "./components/home";
 import AboutView from "./components/about";
 import SearchView from "./components/search";
 
+// TMDB API KEY = 5f20273fd7bb65b5bb1dd8450bf11dd1
+// EXAMPLE REQUEST = https://api.themoviedb.org/3/movie/550?api_key=5f20273fd7bb65b5bb1dd8450bf11dd1
+
 function App() {
 
-  {/* set function-based State for entire app */}
   const [searchResults, setSearchResults] = useState([]);
   const [searchQuery, setSearchQuery] = useState("");
 
+  useEffect(() => {
+    fetch(`https://api.themoviedb.org/3/search/movie?api_key=5f20273fd7bb65b5bb1dd8450bf11dd1&query=${searchQuery}`)
+      .then(response => response.json())
+      .then(data => setSearchResults(data.results))
+  }, [searchQuery]);
+
   return (
     <BrowserRouter>
-      <Navbar />
+      <Navbar searchQuery={searchQuery} setSearchQuery={setSearchQuery} />
       <Routes>
         <Route path="/" element={<HomeView />} />
         <Route path="/about" element={<AboutView />} />
-        <Route path="/search" keyword={searchQuery} searchResults={searchResults} element={<SearchView />} />
+        <Route path="/search" element={<SearchView keyword={searchQuery} searchResults={searchResults} />} />
       </Routes>
     </BrowserRouter>
   );
