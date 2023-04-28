2337c8138231c147b11006cd314fb1f4926b2dc6
Damon Turcotte <damon.j.turcotte@gmail.com>
2023-04-28 01:33:30 -0600

    Map and render movie cards

--- src/components/search.js
+++ src/components/search.js
@@ -1,10 +1,18 @@
 import Hero from './hero';
+import MovieCard from "./movie";
 
 const SearchView = ({ keyword, searchResults }) => {
   const title = `Results for: ${keyword}`;
+  const movies = searchResults.map((movie, index) => <MovieCard key={index} movie={movie} />);
+  
   return (
     <div>
       <Hero text={title} />
+      <div className="container">
+        <div className="d-flex flex-between row">
+          {movies}
+        </div>
+      </div>
     </div>
   )
 }
